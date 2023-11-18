7cfc8c6b49700fdecb3fbfe45e26b2e48068b014
prafullKrRj <prafullkumar384@gmail.com>
2023-11-18 17:45:10 +0530

    OnBoardEnd1

--- app/src/main/java/com/example/contestifyme/features/profileFeature/ui/ProfileViewModel.kt
+++ app/src/main/java/com/example/contestifyme/features/profileFeature/ui/ProfileViewModel.kt
@@ -1,9 +1,7 @@
 package com.example.contestifyme.features.profileFeature.ui
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.contestifyme.features.profileFeature.model.ProfileRepository
-import com.example.contestifyme.ui.OnBoardingVM
 
 class ProfileViewModel(
     private val profileRepository: ProfileRepository,
